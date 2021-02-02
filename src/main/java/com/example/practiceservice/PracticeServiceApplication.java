7162fd634f3f9eb59e7f02ec3121e584489cee50
ashley dorris <ashleydorris89@gmail.com>
2021-02-01 17:39:28 -0800

    dummy

--- src/main/java/com/example/practiceservice/PracticeServiceApplication.java
+++ src/main/java/com/example/practiceservice/PracticeServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PracticeServiceApplication {
 
     public static void main(String[] args) {
+        System.out.println("hello new");
         SpringApplication.run(PracticeServiceApplication.class, args);
     }
 
